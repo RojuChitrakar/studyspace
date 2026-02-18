1e96c37a78e254e2363c6a1481a8d7fb00aed0fc
RojuChitrakar <rojuchit00@gmail.com>
2026-02-18 16:13:11 +0530

    Fix API endpoint for login request

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -21,7 +21,7 @@ function Login() {
     setLoading(true);
 
     try {
-      const res = await api.post("/auth/login", {
+      const res = await api.post("api/auth/login", {
         email,
         password,
       });
